624d5ac5e04cbc40500b2b3e28cbae3703302e45
TimatoPaste <tmthlou@gmail.com>
2023-06-02 10:08:15 -0700

    patch attempt

--- chem-site/src/main.ts
+++ chem-site/src/main.ts
@@ -2,6 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+//need to import parse to use back4app 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
