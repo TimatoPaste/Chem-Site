d84fd0bcbfedbedc1b6d4bb763395b15884cda61
TimatoPaste <tmthlou@gmail.com>
2023-06-03 04:15:29 -0700

    Turned the db writing block into a function!

--- chemWebsite/src/main.ts
+++ chemWebsite/src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app/app.module';
 	platformBrowserDynamic().bootstrapModule(AppModule)
 	  .catch(err => console.error(err));
 
-/*
+
 	//database access reference
 	
 	//parse is for the back4app database
@@ -17,26 +17,28 @@ import { AppModule } from './app/app.module';
 	Parse.initialize('FgJsXpqyLH852BBw5DnuccJOUXLMsysjccWNcBIC', 'h6TiU187AcIgJfQN9sRXildBMvUFiIiBmUP3GtTm');
 	(Parse as any).serverURL = 'https://parseapi.back4app.com/';
 	
-	//extend gets db address of a specific class/list. Makes new class/list if it doesn't exist
-	//can create and write to new classes because they are public by default
-	//auth-only classes will require some ACL config stuff
-	let person = Parse.Object.extend("a");
 	
-	//creates new object in the class/list (adding data)
-	let add = new person();
-	
-	//sets attributes of the data point (attribute, value)
-	add.set("username","bye");
-	
-	//pushes the change to the db
-	add.save();
-	
-	let ask = new Parse.Query(person);
+	function add(resourceName: string, link: string){
+		
+		//extend gets db address of a specific class/list. Makes new class/list if it doesn't exist
+		//can create and write to new classes because they are public by default
+		//auth-only classes will require some ACL config stuff
+		let list = Parse.Object.extend("Links");
+		
+		//creates new object in the class/list (adding data)
+		let add = new list();
+		
+		//sets attributes of the data point (attribute, value)
+		add.set("Link",link.trim());
+		add.set("resourceName",resourceName.trim());
+		
+		//pushes the change to the db
+		add.save();
+	}
+	//let ask = new Parse.Query(person);
 	//ask.get(id); returns the object at this id value in the class
 	
-*/
-	
-	
+//saved some useful stuff in bookmarks to help with ACL and querying + custom object id
 
 	
 	
